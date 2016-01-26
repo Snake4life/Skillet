1dc2ed997df32e91fa18c8a3244faa3f536dac22
Patrick DePippo <pdepip@gmail.com>
2016-01-25 23:29:53 -0500

    faviconserv

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var favicon = require('serve-favicon');
 require('babel-register');
 
 var swig  = require('swig');
@@ -27,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 //app.use('/api', expressJwt({secret: secret}));
 
